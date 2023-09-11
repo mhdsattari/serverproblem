27a9afc3b999066d65cec2c1d333148b01ce0de8
mahdi <a@a.co>
2023-09-11 23:21:06 +0330

    count,sort problem worked! serverip does not work yet..

--- report/urls.py
+++ report/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.serverlist.as_view(),name="server_list"),
-    path('problem/<int:pkk>', views.serverproblem.as_view(),name = "server_problem"),
-    path('ServerCreate',views.ServerCreate.as_view(),name="server_create")    ,
-    path('ServerUpdate/<int:pk>',views.ServerUpdate.as_view(),name="server_update")    
+    path('problem/<int:pkk>', views.problem.as_view(),name = "server_problem"),
+    path('ServerCreate',views.ServerCreate.as_view(),name="server_create"),
+    path('ServerUpdate/<int:pk>',views.ServerUpdate.as_view(),name="server_update"),
+    path('p/<int:serverip>',views.serversearch.as_view(),name="server_search")
 ]
--- report/views.py
+++ report/views.py
@@ -1,5 +1,8 @@
 from typing import Any
+from django.db import models
 from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
@@ -7,19 +10,33 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.contrib.auth.decorators import permission_required
-
+from django.db.models import Count
 from .models import Server,Problem
 
-
-class serverlist(PermissionRequiredMixin,ListView):
-    permission_required = "report | server | can add server"
+class serversearch(DetailView):
+    model = Server
+    context_object_name = 'server'
+    template_name = "report/server.html"
+    def get_queryset(self):
+        print ('5555555555555555555555555555555')
+        return Server.objects.get(id=self.request.resolver_match.kwargs['serverip'])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['server'] = Server.objects.get(id=self.request.resolver_match.kwargs['serverip'])
+        return context
+    
+class serverlist(LoginRequiredMixin,ListView):
     model = Server
     paginate_by = 50
     context_object_name = "servers"
     template_name = "report/index.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['servers'] = Server.objects.filter(user = self.request.user).annotate(problemcount=Count('problem')).order_by('-problemcount')
+        return context
     
 
-class serverproblem(LoginRequiredMixin,ListView):
+class problem(LoginRequiredMixin,ListView):
     model = Problem
     context_object_name = "problems"
     template_name = "report/problem.html"
@@ -37,9 +54,15 @@ class ServerCreate(LoginRequiredMixin,CreateView):
     fields = "__all__"
     template_name = "report/ServerCreate.html"
     success_url = reverse_lazy("server_list")
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class ServerUpdate(LoginRequiredMixin,UpdateView):
     model = Server
     fields = "__all__"
     template_name = "report/ServerUpdate.html"
     success_url = reverse_lazy("server_list")
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
